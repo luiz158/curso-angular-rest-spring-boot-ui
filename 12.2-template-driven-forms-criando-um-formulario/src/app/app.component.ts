fbf9da2ac24d084a289310922e44ba2354d5ce3a
wellingtonx <wellingtonex@gmail.com>
2018-12-29 20:26:20 -0200

    brincando com formularios com template forms

--- 12.2-template-driven-forms-criando-um-formulario/src/app/app.component.ts
+++ 12.2-template-driven-forms-criando-um-formulario/src/app/app.component.ts
@@ -1,6 +1,12 @@
 import { NgForm } from '@angular/forms';
 import { Component } from '@angular/core';
 
+class cliente {
+  nome: string;
+  email: string;
+  profissao: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,8 +14,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  cliente = new cliente();
+
+  profissoes = [
+    {codigo: 1, descricao: 'Programador'},
+    {codigo: 2, descricao: 'Puta'},
+    {codigo: 3, descricao: 'Gerente'},
+    {codigo: 4, descricao: 'Outra'}
+   ];
+
   salvar(form: NgForm) {
-    console.log(form);
+    console.log(form.value);
+    // this.cliente.nome = form.value.nome;
+    // this.cliente.email = form.value.email;
+    // this.cliente.profissao = form.value.profissao;
+    
   }
 
 }
